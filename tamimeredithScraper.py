e9b235a6b4eb82c26a7d39f953fb8266aeeb13df
dylanpomeroy <dylan.pomeroy@dal.ca>
2017-05-04 01:32:14 -0300

    Added static variable specification at top; placed in infinite while so that while loop isn't needed in bash

--- tamimeredithScraper.py
+++ tamimeredithScraper.py
@@ -1,26 +1,32 @@
 from lxml import html
 import requests
 import urllib
+import time
 
-page = requests.get('http://www.tamimeredith.ca/academic/csci-3136/')
-tree = html.fromstring(page.content)
+pathToStoreFiles = "/var/www/html/schoolScraper/3136/"
+sleepTimeBetweenScraping = 290 #seconds
 
-items = tree.xpath(
-    "body"
-    "/div[@id='wrapper']"
-    "/div[@id='main']"
-    "/div[@id='container']"
-    "/div[@id='content']"
-    "/div[@id='post-927']"
-    "/div[@class='entry-content']"
-    "/ul"
-    "/item"
-    "/a")
+while(True):
+    page = requests.get('http://www.tamimeredith.ca/academic/csci-3136/')
+    tree = html.fromstring(page.content)
 
-for i, item in enumerate(items):
-    url = str(item.xpath("@href")[0])
-    name = url.split('/')[-1]
-    try:
-        urllib.request.urlretrieve(url, "/var/www/html/schoolScraper/3136/"+name)
-    except urllib.error.URLError:
-        pass
+    items = tree.xpath(
+        "body"
+        "/div[@id='wrapper']"
+        "/div[@id='main']"
+        "/div[@id='container']"
+        "/div[@id='content']"
+        "/div[@id='post-927']"
+        "/div[@class='entry-content']"
+        "/ul"
+        "/item"
+        "/a")
+
+    for i, item in enumerate(items):
+        url = str(item.xpath("@href")[0])
+        name = url.split('/')[-1]
+        try:
+            urllib.request.urlretrieve(url, pathToStoreFiles+name)
+        except urllib.error.URLError:
+            pass
+    time.sleep(sleepTimeBetweenScraping)
