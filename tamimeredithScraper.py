3fd05526402910f3cd132d854317ef340160c392
dylanpomeroy <dylan.pomeroy@dal.ca>
2017-05-03 22:48:48 -0300

    first commit

---
+++ tamimeredithScraper.py
@@ -0,0 +1,26 @@
+from lxml import html
+import requests
+import urllib
+
+page = requests.get('http://www.tamimeredith.ca/academic/csci-3136/')
+tree = html.fromstring(page.content)
+
+items = tree.xpath(
+    "body"
+    "/div[@id='wrapper']"
+    "/div[@id='main']"
+    "/div[@id='container']"
+    "/div[@id='content']"
+    "/div[@id='post-927']"
+    "/div[@class='entry-content']"
+    "/ul"
+    "/item"
+    "/a")
+
+for i, item in enumerate(items):
+    url = str(item.xpath("@href")[0])
+    name = url.split('/')[-1]
+    try:
+        urllib.request.urlretrieve(url, name)
+    except urllib.error.URLError:
+        pass
